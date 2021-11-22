42669e4e7d875aeb5183a9ca341df9e3be5e42de
saka6502 <saka6502@gmail.com>
2021-11-22 22:16:49 +0900

    [W1] 카드 추가 및 디자인 변경

--- week 1-Jetpack Compose basics/BasicCodelab-may/app/src/main/java/com/example/basiccodelabew2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodelab-may/app/src/main/java/com/example/basiccodelabew2/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
@@ -17,7 +19,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccodelabew2.ui.theme.BasicCodelabeW2Theme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
@@ -53,37 +60,40 @@ fun OnBoardingScreen(onContinueClicked: () -> Unit) {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        val expanded = remember {
-            mutableStateOf(false)
-        }
-        val extraPadding by animateDpAsState(
-            if (expanded.value) 48.dp else 0.dp,
-            animationSpec = spring(
-                dampingRatio = Spring.DampingRatioMediumBouncy,
-                stiffness = Spring.StiffnessLow
-            )
-        )
+        CardContent(name)
+    }
+}
 
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello,")
-                Text(text = name, style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                ))
+@Composable
+fun CardContent(name: String) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    Row(modifier = Modifier.padding(24.dp).animateContentSize()) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Hello,")
+            Text(text = name, style = MaterialTheme.typography.h4.copy(
+                fontWeight = FontWeight.ExtraBold
+            ))
+            if (expanded) {
+                Text(text = ("Composem ipsum color sit lazy, " +
+                        "padding theme elit, sed do bouncy. ").repeat(4))
             }
-            OutlinedButton(onClick = {
-                expanded.value = !expanded.value
-            }) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
