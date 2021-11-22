9926452e0c4b0f73526cd16bc2a8b39f8cc540af
saka6502 <saka6502@gmail.com>
2021-11-22 21:39:20 +0900

    [W1] 아이템 접혔다 펼쳐졌을 때 애니메이션 추가

--- week 1-Jetpack Compose basics/BasicCodelab-may/app/src/main/java/com/example/basiccodelabew2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodelab-may/app/src/main/java/com/example/basiccodelabew2/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.basiccodelabew2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
@@ -55,13 +58,19 @@ fun Greeting(name: String) {
         val expanded = remember {
             mutableStateOf(false)
         }
-        val extraPadding = if (expanded.value) 48.dp else 0.dp
+        val extraPadding by animateDpAsState(
+            if (expanded.value) 48.dp else 0.dp,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )
 
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
